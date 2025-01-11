c6ce86c81018edc8a4c585a3cabba7a664e98fa4
EctobiusRexV <marow6@ulaval.ca>
2025-01-11 08:29:14 -0500

    wall positions are -1, else is 0

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import random
 from game_message import *
+import numpy as np
 
 
 class Bot:
     def __init__(self):
         print("Initializing your super mega duper bot")
+        self.wall_positions = []
 
     def get_next_move(self, game_message: TeamGameState):
         """
@@ -12,19 +14,18 @@ class Bot:
         """
         actions = []
 
-        for character in game_message.yourCharacters:
-            actions.append(
-                random.choice(
-                    [
-                        MoveUpAction(characterId=character.id),
-                        MoveRightAction(characterId=character.id),
-                        MoveDownAction(characterId=character.id),
-                        MoveLeftAction(characterId=character.id),
-                        GrabAction(characterId=character.id),
-                        DropAction(characterId=character.id),
-                    ]
-                )
-            )
+        if game_message.tick == 1:
+            for line in range(len(game_message.map.tiles)):
+                for column in range(len(game_message.map.tiles[line])):
+                    if game_message.map.tiles[line][column] == TileType.WALL:
+                        self.wall_positions.append((line, column))
+
+
+        grid = np.zeros((game_message.map.width, game_message.map.height))
+        grid_size = (game_message.map.width, game_message.map.height)
+
+        for wall in self.wall_positions:
+            grid[wall] = -1
 
         # You can clearly do better than the random actions above! Have fun!
         return actions
